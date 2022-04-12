58d7ab77ac0ec37c3bf3611d2a38f9325391e500
Dini Hanifia <dinihanifiaa@gmail.com>
2022-04-12 22:57:31 +0700

    dini

---
+++ src/main/java/com/bekennft/BekenNftApplication.java
@@ -0,0 +1,13 @@
+package com.bekennft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BekenNftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BekenNftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bekennft/BekenNftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekennft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BekenNftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
